2224bb8ebf31d8f81b709227ccfe7777822da7a8
KRISHNAMURTHY-DXC, ARAVINDA <aravinda.krishnamurthy-dxc@hpe.com>
2019-09-14 14:40:50 +0530

    updated

--- src/main/java/org/dxc/ngoi/order/receiver/ReceiveXcblOrderApplication.java
+++ src/main/java/org/dxc/ngoi/order/receiver/ReceiveXcblOrderApplication.java
@@ -1,16 +1,23 @@
 package org.dxc.ngoi.order.receiver;
 
+import java.io.FileOutputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 public class ReceiveXcblOrderApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		
+		FileCopyUtils.copy(new ClassPathResource("server.truststore.jks").getInputStream(),
+				new FileOutputStream("/tmp/server.truststore.jks"));
 		SpringApplication.run(ReceiveXcblOrderApplication.class, args);
 	}
 
